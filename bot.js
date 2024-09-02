5922f2f5507892dd7119b4ec627df2dcab2143b5
Ogboh Chinonso Benjamin <Ogbohchinonso@gmail.com>
2024-09-03 00:54:43 +0100

    downgrade

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 import TelegramBot from 'node-telegram-bot-api';
 import fetch from 'node-fetch';
 
-const play = 15;
+const play = 5;
 const botToken = "6301224962:AAH0xlCXPsxeUpBAaN8vJqiDhwOO7b-C9Yw"; // Replace with your Telegram Bot Token
 
 const bot = new TelegramBot(botToken, { polling: true });
